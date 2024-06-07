857f30aa02455dd32b2b1829144f1f0bf5e5246b
asif331001 <asif95331001@gmail.com>
2024-06-07 12:16:55 +0600

    first commit

---
+++ lib/themes/appbar_style.dart
@@ -0,0 +1,10 @@
+
+import 'package:assignment_contact_list/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class AppbarStyle {
+  static AppBarTheme getAppBarTheme() => const AppBarTheme(
+      backgroundColor: appPrimaryColor,
+      foregroundColor: whiteColor,
+      titleTextStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold));
+}
---
+++ lib/themes/elevated_button_style.dart
@@ -0,0 +1,19 @@
+
+import 'package:assignment_contact_list/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class ElevatedButtonStyle{
+  static ElevatedButtonThemeData getElevatedButtonStyle() => ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: appPrimaryColor,
+      foregroundColor: whiteColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5),
+      ),
+      textStyle: const TextStyle(
+        fontSize: 17,
+        fontWeight: FontWeight.bold
+      )
+    )
+  );
+}
